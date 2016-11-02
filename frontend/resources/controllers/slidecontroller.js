75a5ec6cfe39ec72ebe04931e99224821fb265f6
Michael Glenn <dmichaelglenn@gmail.com>
2016-11-02 12:20:09 -0700

    changing things for portfolio display

--- frontend/resources/controllers/slidecontroller.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("MainApp");
-
-app.controller("SlideController", function($scope) {
-    console.log("hitting the slide controller");
-    $scope.questionTest = "This is what the question looks like. i'm typing a few sentences to see how long blocks of text lay on the page";
-    $scope.answer1 = "This is the first answer. Some of them are multiple sentences long, so I want to be sure it fits on the button.";
-    $scope.answer2 = "Some are short and need to look good too";
-    $scope.direct1 = "#/directtest"
-});
--- frontend/services/authService.js
+++ frontend/services/authService.js
@@ -1,65 +1,58 @@
 var app = angular.module("MainApp");
 
-app.service("LoginService", ["$http", "$location", "TokenService", "AuthInterceptor", function($http, $location, TokenService, AuthInterceptor){
-  
-//login service to add a new user. Takes a new user object
-  this.user = {};
-  
-  this.login = function(userObj){
-    console.log("loggin in: " + userObj)
-    return $http.post("/auth/login", userObj).then(function(response) {
-      console.log(response.data);
-      this.user = response.data.user;
-      TokenService.setToken(response.data.token);
-      $location.path("/q1");
-      return response.data
-    })
-  }
-  
-  this.signup = function(userObj){
-    console.log("sending" + userObj)
-    return $http.post("/auth/signup", userObj).then(function(response) {
-      return response.data
-      console.log(response.data)
-    })
-  }
-  
-  this.logout = function() {
-    TokenService.removeToken();
+app.service("LoginService", ["$http", "$location", "TokenService", "AuthInterceptor", function($http, $location, TokenService, AuthInterceptor) {
+
+    //login service to add a new user. Takes a new user object
     this.user = {};
-  }
-  
-  this.isAuthenticated = function() {
-    return !!TokenService.getToken();
-  }
-  
+
+    this.login = function(userObj) {
+        // console.log("loggin in: " + userObj)
+        return $http.post("/auth/login", userObj).then(function(response) {
+            // console.log(response.data);
+            this.user = response.data.user;
+            TokenService.setToken(response.data.token);
+            $location.path("/q1");
+            return response.data;
+        });
+    };
+
+    this.signup = function(userObj) {
+        // console.log("sending" + userObj);
+        return $http.post("/auth/signup", userObj).then(function(response) {
+            return response.data;
+        });
+    };
+
+    this.logout = function() {
+        TokenService.removeToken();
+        this.user = {};
+    };
+
+    this.isAuthenticated = function() {
+        return !!TokenService.getToken();
+    };
+
 }]); //close service
 
-app.factory("AuthInterceptor", function($q, $location, TokenService){
-  return {
-    request: function(config) {
-      var token = TokenService.getToken();
-      if (token) {
-        config.headers = config.headers || {};
-        config.headers.authorization = "Bearer " + token;
-      }
-      return config;
-    },
-    responseError: function(response) {
-      if (response.status ===401) {
-        TokenService.removeToken();
-      }
-      return $q.reject(response);
-    }
-  }
+app.factory("AuthInterceptor", function($q, $location, TokenService) {
+    return {
+        request: function(config) {
+            var token = TokenService.getToken();
+            if (token) {
+                config.headers = config.headers || {};
+                config.headers.authorization = "Bearer " + token;
+            }
+            return config;
+        },
+        responseError: function(response) {
+            if (response.status === 401) {
+                TokenService.removeToken();
+            }
+            return $q.reject(response);
+        }
+    };
 });
 
-app.config(function($httpProvider){
-  $httpProvider.interceptors.push("AuthInterceptor");
+app.config(function($httpProvider) {
+    $httpProvider.interceptors.push("AuthInterceptor");
 });
-
-
-
-
-
-
