744853dd51e3ce96e7b222f215ed8728e28f5494
Michael Glenn <dmichaelglenn@gmail.com>
2016-11-03 14:12:40 -0700

    preparing for deployment

--- frontend/resources/controllers/q10controller.js
+++ frontend/resources/controllers/q10controller.js
@@ -3,7 +3,8 @@ var app = angular.module("MainApp");
 app.controller("Q10Controller", ['$scope', 'ReqService', function($scope, ReqService) {
     ReqService.addPoint();
     ReqService.sendPoints();
-    $scope.question = "You kill your dad and abscond with your mother to the Florida Keys. You have succumbed to the OEDIPUS CYCLE.";
+    $scope.finalScore = ReqService.scoreCount;
+    $scope.question = "You kill your dad and abscond with your mother to the Florida Keys. You have succumbed to the OEDIPUS CYCLE. Your final score is: " + $scope.finalScore;
     $scope.answer1 = "Play Again";
     $scope.answer2 = "There is no escape from your unending subconcious torment";
     $scope.direct1 = "#/q1";
